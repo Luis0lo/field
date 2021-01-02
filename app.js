810a4d6f160ee51aae818e4f247aee7749318f17
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-01-02 18:39:59 +0000

    seeds..

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Field = require('./models/field');
+
+mongoose.connect('mongodb://localhost:27017/field', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+// check if there is any error
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  //   res.send('Field N1');
   res.render('home');
+});
+
+app.get('/reportfield', async (req, res) => {
+  const camp = new Field({
+    title: 'Coelho',
+    description: 'Aguacate e Anonas',
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
