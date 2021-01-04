a5fdc536b69bb731e278993a601aecf9d0e2c57a
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-01-04 18:50:59 +0000

    error.handler

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utilities/catchAsync');
+const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
 const Field = require('./models/field');
 
@@ -32,41 +34,70 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.get('/fields', async (req, res) => {
-  const fields = await Field.find({});
-  res.render('fields/index', { fields });
-});
+app.get(
+  '/fields',
+  catchAsync(async (req, res) => {
+    const fields = await Field.find({});
+    res.render('fields/index', { fields });
+  })
+);
 
 app.get('/fields/new', (req, res) => {
   res.render('fields/new');
 });
 
-app.post('/fields', async (req, res) => {
-  const field = new Field(req.body.field);
-  await field.save();
-  res.redirect(`/fields/${field._id}`);
+app.post(
+  '/fields',
+  catchAsync(async (req, res, next) => {
+    if (!req.body.field) throw new ExpressError('Invalid Field Data', 400);
+    const field = new Field(req.body.field);
+    await field.save();
+    res.redirect(`/fields/${field._id}`);
+  })
+);
+
+app.get(
+  '/fields/:id',
+  catchAsync(async (req, res) => {
+    const field = await Field.findById(req.params.id);
+    res.render('fields/show', { field });
+  })
+);
+
+app.get(
+  '/fields/:id/edit',
+  catchAsync(async (req, res) => {
+    const field = await Field.findById(req.params.id);
+    res.render('fields/edit', { field });
+  })
+);
+
+app.put(
+  '/fields/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const field = await Field.findByIdAndUpdate(id, { ...req.body.field });
+    res.redirect(`/fields/${field._id}`);
+  })
+);
+
+app.delete(
+  '/fields/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Field.findByIdAndDelete(id);
+    res.redirect('/fields');
+  })
+);
+
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page not found', 404));
 });
 
-app.get('/fields/:id', async (req, res) => {
-  const field = await Field.findById(req.params.id);
-  res.render('fields/show', { field });
-});
-
-app.get('/fields/:id/edit', async (req, res) => {
-  const field = await Field.findById(req.params.id);
-  res.render('fields/edit', { field });
-});
-
-app.put('/fields/:id', async (req, res) => {
-  const { id } = req.params;
-  const field = await Field.findByIdAndUpdate(id, { ...req.body.field });
-  res.redirect(`/fields/${field._id}`);
-});
-
-app.delete('/fields/:id', async (req, res) => {
-  const { id } = req.params;
-  await Field.findByIdAndDelete(id);
-  res.redirect('/fields');
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Something went wrong';
+  res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
