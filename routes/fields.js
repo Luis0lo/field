693171d25e7a34fae7a11a4cd03656e33c41efc1
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-01-18 15:32:54 +0000

    routes.fancy.restructure

--- routes/fields.js
+++ routes/fields.js
@@ -3,27 +3,21 @@ const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
 const Field = require('../models/field');
 const { isLoggedIn, isOwner, validateField } = require('../middleware');
-
 const fields = require('../controllers/fields');
 
-router.get('/', catchAsync(fields.index));
+router
+  .route('/')
+  .get(catchAsync(fields.index))
+  .post(isLoggedIn, validateField, catchAsync(fields.createField));
 
 router.get('/new', isLoggedIn, fields.renderNewForm);
 
-router.post('/', isLoggedIn, validateField, catchAsync(fields.createField));
-
-router.get('/:id', catchAsync(fields.showField));
+router
+  .route('/:id')
+  .get(catchAsync(fields.showField))
+  .put(isLoggedIn, isOwner, validateField, catchAsync(fields.updateFields))
+  .delete(isLoggedIn, isOwner, catchAsync(fields.deleteField));
 
 router.get('/:id/edit', isLoggedIn, isOwner, catchAsync(fields.renderEditForm));
-
-router.put(
-  '/:id',
-  isLoggedIn,
-  isOwner,
-  validateField,
-  catchAsync(fields.updateFields)
-);
-
-router.delete('/:id', isLoggedIn, isOwner, catchAsync(fields.deleteField));
 
 module.exports = router;
