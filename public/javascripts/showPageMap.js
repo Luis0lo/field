68542f6535f478305cc0ec9baf9fb93be331cdad
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-01-19 16:43:22 +0000

    centering.map.on.field.location

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,6 +2,8 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 4, // starting zoom
+  center: field.geometry.coordinates, // starting position [lng, lat]
+  zoom: 9, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat(field.geometry.coordinates).addTo(map);
