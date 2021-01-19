55abf269b17f0ea7acd3b4a6fdf62d5b460efde3
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-01-19 04:02:07 +0000

    show.img.index.edit.seeds

--- seeds/index.js
+++ seeds/index.js
@@ -27,10 +27,21 @@ const seedDB = async () => {
       owner: '6001b3c9e594dd2f000e745f',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/4025682',
       description:
         'Agriculture is the science and art of cultivating plants and livestock.[1] Agriculture was the key development in the rise of sedentary human civilization, whereby farming of domesticated species created food surpluses that enabled people to live in cities. The history of agriculture began thousands of years ago. After gathering wild grains beginning at least 105,000 years ago, nascent farmers began to plant them around 11,500 years ago. Pigs, sheep and cattle were domesticated over 10,000 years ago. Plants were independently cultivated in at least 11 regions of the world. Industrial agriculture based on large-scale monoculture in the twentieth century came to dominate agricultural output, though about 2 billion people still depended on subsistence agriculture into the twenty-first.',
       price: price,
+      images: [
+        {
+          url:
+            'https://res.cloudinary.com/luis0lo/image/upload/v1611028143/field/fg23h1hspkahoiormmjp.jpg',
+          filename: 'field/fg23h1hspkahoiormmjp',
+        },
+        {
+          url:
+            'https://res.cloudinary.com/luis0lo/image/upload/v1611028143/field/zgesxlcxtmazht0gmals.png',
+          filename: 'field/zgesxlcxtmazht0gmals',
+        },
+      ],
     });
     await camp.save();
   }
