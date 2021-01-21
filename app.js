4e7ba3f8e27749c9bc6e65824fe68621dd844a7e
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-01-21 21:13:36 +0000

    ready to deploy.2

--- app.js
+++ app.js
@@ -163,6 +163,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-  console.log('Running in 3k');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Running in ${port}`);
 });
